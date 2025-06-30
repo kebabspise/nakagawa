97b8bafec9437da3ae433d62ece4483e4957b9b0
Kazuya Nakagawa <kazuya.nakagawa@sato-global.com>
2025-06-30 09:05:00 +0900

    store

--- asp-nakagawa/asp-nakagawa/obj/Debug/net8.0/asp-nakagawa.AssemblyInfo.cs
+++ asp-nakagawa/asp-nakagawa/obj/Debug/net8.0/asp-nakagawa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("asp-nakagawa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd84d5a89129a01a25afd2aa7ac181cf08df7af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+923f913605cfe0237773504adf0813446dafe05a")]
 [assembly: System.Reflection.AssemblyProductAttribute("asp-nakagawa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("asp-nakagawa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
